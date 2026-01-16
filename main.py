08d1add75f9acc643d1c6b6ec62a8db63e07f09e
JonBaldwinAlmarez <almarezjbm@gmail.com>
2026-01-16 18:12:24 +0800

    Add cors middleware

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.routes.issues import router as issues_router
 from app.middleware.timer import timer_middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # Create an instance of the FastAPI application
 app = FastAPI()
-app.middleware("http")(timer_middleware)
+app.middleware("http")(timer_middleware)  # Add the timer middleware to the app
 app.include_router(issues_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
